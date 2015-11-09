78174637bd5e7779a572549a41aa19805ae10d0f
kreikenbaum <kreikenbaum@users.noreply.github.com>
2015-11-09 17:26:39 +0100

    had no counter split from extract, had less on a-sys code, touch ch 2, (python), (ml-probability theory)

---
+++ bin/counter.py
@@ -0,0 +1,245 @@
+'''aggregates trace data, extracts features'''
+import collections
+import logging
+import json
+import math
+import os
+import subprocess
+
+HOME_IP = '134.76.96.47' #td: get ips
+LOGLEVEL = logging.DEBUG
+# LOGLEVEL = logging.INFO
+TIME_SEPARATOR = '@'
+
+def _append_features(keys, filename):
+    '''appends features in trace file "filename" to keys, indexed by domain'''
+    domain = _get_domain(filename)
+    if not keys.has_key(domain):
+        keys[domain] = []
+    keys[domain].append(Counter.from_pcap(filename))
+
+def discretize(number, by):
+    '''discretizes number by increment
+    >>> discretize(15, 3)
+    15
+    >>> discretize(15, 2)
+    14
+    '''
+    return (number / by) * by
+
+def _get_domain(filename):
+    '''extracts domain part from filename:
+    >>> _get_domain('/tmp/google.com@12412')
+    'google.com'
+    '''
+    return os.path.basename(filename).split(TIME_SEPARATOR)[0]
+
+def pad(row, upto=300):
+    '''enlarges row to have upto entries (padded with 0)
+    >>> _pad([2], 20)
+    [2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    '''
+    row.extend([0] * (upto - len(row)))
+    return row
+
+def sum_bytes(packets):
+    '''sum of (incoming, outgoing) bytes
+    >>> sum_bytes([3,4,-1,-4])
+    (5, 7)
+    '''
+    return((sum((x for x in packets if x < 0)),
+            sum((x for x in packets if x > 0))))
+
+def sum_packets(packets):
+    '''number of (incoming, outgoing) bytes
+    >>> sum_packets([3,4,-1,-4])
+    (2, 2)
+    '''
+    return((sum((1 for x in packets if x < 0)),
+            sum((1 for x in packets if x > 0))))
+
+def _sum_stream(packets):
+    '''sums adjacent packets in the same direction
+    >>> _sum_stream([10, -1, -2, 4])
+    [10, -3, 4]
+    '''
+    tmp = 0
+    out = []
+    for size in packets:
+        if size > 0 and tmp < 0 or size < 0 and tmp > 0:
+            out.append(tmp)
+            tmp = size
+        else:
+            tmp += size
+    out.append(tmp)
+    return out
+
+def _sum_numbers(packets):
+    '''sums number of adjacent packets in the same direction, grouped by
+    1,2,3-5,6-8,9-13,14
+    >>> _sum_numbers([10, -1, -2, 4])
+    [1, 2, 1]
+    '''
+    counts = _sum_stream([math.copysign(1, x) for x in packets])
+    dictionary = {1: 1, 2: 2, 3: 3, 4: 3, 5: 3, 6: 4, 7: 4, 8:4,
+                  9:5, 10:5, 11:5, 12:5, 13:5, 14:6}
+    return [dictionary[abs(x)] for x in counts]
+
+class Counter(object):
+    '''single trace file, also helper functions to create counters for all
+    files in directory'''
+    def __init__(self):
+        self.fixed = None
+        self.variable = None
+        self.packets = []
+        self.timing = []
+        self.warned = False
+
+    def __str__(self):
+        return 'p: {}'.format(self.panchenko())
+
+    @classmethod
+    def from_json(cls, jsonstring):
+        '''creates Counter from self.to_json'''
+        tmp = Counter()
+
+        for key, value in json.loads(jsonstring).iteritems():
+            setattr(tmp, key, value)
+        return tmp
+
+    @staticmethod
+    def save(counter_dict):
+        '''saves counters as json data to file'''
+        for k, per_domain in counter_dict.iteritems():
+            if not per_domain:
+                continue
+            with open(k + '.json', 'w') as file_:
+                logging.debug('writing json for %s', k)
+                for counter in per_domain:
+                    file_.write(counter.to_json())
+                    file_.write('\n')
+
+    @staticmethod
+    def all_from_json(filename):
+        '''returns all the counters in json file named filename '''
+        out = []
+        for entry in open(filename):
+            out.append(Counter.from_json(entry))
+        return out
+
+    @staticmethod
+    def all_from_dir(dirname):
+        '''all packets traces in subdirectories of the name domain@tstamp are
+        parsed to Counters. If there are JSON files created by
+        save, use those instead of the pcap files for their domains'''
+        out = {}
+
+        for (dirpath, _, filenames) in os.walk(dirname):
+            for jfile in [x for x in filenames if '.json' in x]:
+                domain = jfile.replace('.json', '')
+                logging.debug('processing %s', jfile)
+                out[domain] = Counter.all_from_json(jfile)
+                filenames.remove(jfile)
+                for trace in [x for x in filenames if domain + '@' in x]:
+                    filenames.remove(trace)
+
+            length = len(filenames)
+            for i, filename in enumerate(filenames):
+                fullname = os.path.join(dirpath, filename)
+                if TIME_SEPARATOR in fullname: # file like google.com@1445350513
+                    logging.info('processing (%d/%d) %s ',
+                                 i+1, length, fullname)
+                    _append_features(out, fullname)
+
+        return out
+
+    @classmethod
+    def from_pcap(cls, filename):
+        '''creates Counter from pcap file'''
+        tmp = Counter()
+
+        tshark = subprocess.Popen(args=['tshark', '-r' + filename],
+                                  stdout=subprocess.PIPE)
+        for line in iter(tshark.stdout.readline, ''):
+            logging.debug(line)
+            try:
+                (_, time, src, _, dst, proto, size, rest) = line.split(None, 7)
+            except ValueError:
+                tmp.warned = True
+                logging.warn('file: %s had problems in line \n%s\n',
+                             filename, line)
+                break
+            else:
+                if not 'Len=0' in rest and not proto == 'ARP':
+                    tmp.extract_line(src, size, time)
+                logging.debug('from %s to %s: %s bytes', src, dst, size)
+        return tmp
+
+    def panchenko(self):
+        '''returns panchenko feature vector, (html marker, bytes in, bytes
+        out, total size, percentage incoming, packets in, packets out,
+        size markers)'''
+        self._postprocess()
+        # todo: pad variable
+        return self.fixed + pad(self.variable['size_markers'])
+
+    def extract_line(self, src, size, tstamp):
+        '''aggregates stream values'''
+        if src == HOME_IP: # outgoing packet
+            self.packets.append(int(size))
+            self.timing.append((tstamp, int(size)))
+        elif src: #incoming
+            self.packets.append(- int(size))
+            self.timing.append((tstamp, -int(size)))
+
+    def _postprocess(self):
+        '''sums up etc collected features'''
+        if self.fixed is not None:
+            return
+
+        self.fixed = []
+        self.variable = {}
+
+        self.variable['size_markers'] = [discretize(size, 600) for
+                                         size in _sum_stream(self.packets)]
+        # html response marker, (request must have been sent before)
+        if self.variable['size_markers'][1] < 0:
+            self.fixed.append(self.variable['size_markers'][1])
+        else:
+            self.fixed.append(self.variable['size_markers'][2])
+        # size_incoming size_outgoing
+        self.fixed.extend([discretize(x, 10000) for x in
+                           sum_bytes(self.packets)])
+        # number of unique packet sizes
+        self.fixed.append(discretize(len(set(self.packets)), 2))
+
+        (packets_in, packets_out) = sum_packets(self.packets)
+        # percentage incoming packets
+        self.fixed.append(discretize(100 * packets_in/(packets_in + packets_out),
+                                     5))
+        # number incoming
+        self.fixed.append(discretize(packets_in, 15))
+        # number outgoing
+        self.fixed.append(discretize(packets_out, 15))
+        # all packets as of "A Systematic ..." svm.py code
+        self.variable['all_packets'] = self.packets
+
+    def to_json(self):
+        '''prints packet trace to json, for reimport'''
+        return json.dumps(self.__dict__)
+
+    @staticmethod
+    def from_(*args):
+        '''helper method to handle empty argument'''
+        if len(args) > 1:
+            out = {}
+            for filename in args[1:]:
+                _append_features(out, filename)
+        else:
+            out = Counter.all_from_dir('.')
+        return out
+
+if __name__ == "__main__":
+    logging.basicConfig(format='%(levelname)s:%(message)s', level=LOGLEVEL)
+    from sys import argv
+    counters = Counter.from_(argv)
--- bin/extract_attribute.py
+++ bin/extract_attribute.py
@@ -1,207 +1,54 @@
 #! /usr/bin/env python
+'''extracts (panchenko's) features from pcap and analyses them'''
 
-# extracts (panchenko's) features from pcap and analyses them
-
-import json
+import doctest
 import logging
-import math
-import os
-import os.path
 import numpy as np
 from sklearn import svm, neighbors, cross_validation
-import subprocess
-from sys import argv
+import sys
 
-HOME_IP='134.76.96.47'
-#LOGLEVEL=logging.DEBUG
-LOGLEVEL=logging.INFO
-#LOGLEVEL=logging.WARN
-TIME_SEPARATOR='@'
+import counter
 
-def _append_features(keys, filename):
-    '''appends features in trace file "filename" to keys, indexed by domain'''
-    domain = os.path.basename(filename).split(TIME_SEPARATOR)[0]
-    if not keys.has_key(domain):
-        keys[domain] = []
-    keys[domain].append(Counter.from_pcap(filename))
+LOGLEVEL = logging.DEBUG
+#LOGLEVEL = logging.INFO
+#LOGLEVEL = logging.WARN
+TIME_SEPARATOR = '@'
 
-def _enlarge(row, upto):
+
+def _pad(row, upto):
     '''enlarges row to have upto entries (padded with 0)
-    >>> _enlarge([2], 20)
+    >>> _pad([2], 20)
     [2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
     '''
-    row.extend([0 for i in range(upto - len(row))])
+    row.extend([0] * (upto - len(row)))
     return row
-
-def _sum_stream(packets):
-    '''sums adjacent packets in the same direction
-    >>> _sum_stream([10, -1, -2, 4])
-    [10, -3, 4]
-    '''
-    tmp = 0
-    out = []
-    for size in packets:
-        if size > 0 and tmp < 0 or size < 0 and tmp > 0:
-            out.append(tmp)
-            tmp = size
-        else:
-            tmp += size
-    out.append(tmp)
-    return out
-
-def _sum_numbers(packets):
-    '''sums number of adjacent packets in the same direction, grouped by
-    1,2,3-5,6-8,9-13,14
-    >>> _sum_numbers([10, -1, -2, 4])
-    [1, 2, 1]
-    '''
-    counts = _sum_stream([math.copysign(1, x) for x in packets])
-    dictionary = {1: 1, 2: 2, 3: 3, 4: 3, 5: 3, 6: 4, 7: 4, 8:4,
-                  9:5, 10:5, 11:5, 12:5, 13:5, 14:6}
-    return [dictionary[abs(x)] for x in counts]
-
-
-class Counter:
-    def __init__(self):
-        self.bytes_in = 0
-        self.bytes_out = 0
-        self.packets_in = 0
-        self.packets_out = 0
-        self.packets = []
-        self.timing = []
-        self.warned = False
-
-    @classmethod
-    def from_json(cls, jsonstring):
-        '''creates Counter from self.to_json'''
-        tmp = Counter()
-
-        for key, value in json.loads(jsonstring).iteritems():
-            setattr(tmp, key, value)
-        return tmp
-        
-    @classmethod
-    def from_pcap(cls, filename):
-        '''creates Counter from pcap file'''
-        tmp = Counter()
-    
-        tshark = subprocess.Popen(args=['tshark','-r' + filename],
-                                  stdout=subprocess.PIPE);
-        for line in iter(tshark.stdout.readline, ''):
-            logging.debug(line)
-            try:
-                (x, tstamp, src, y, dst, proto, size, rest) = line.split(None, 7)
-            except ValueError:
-                tmp.warned = True
-                logging.warn('file: %s had problems in line \n%s\n', filename, line)
-                break
-            else:
-                if not 'Len=0' in rest and not proto == 'ARP':
-                    tmp._extract_line(src, size, tstamp)
-                logging.debug('from %s to %s: %s bytes', src, dst, size)
-        return tmp
-
-    def panchenko(self):
-        '''returns panchenko feature vector, (html marker, bytes in, bytes
-        out, total size, percentage incoming, packets in, packets out,
-        size markers)'''
-        self._postprocess()
-        return ([self.html_marker,
-                 self.total_transmitted_bytes_in,
-                 self.total_transmitted_bytes_out,
-                 self.occuring_packet_sizes,
-                 self.percentage,
-                 self.number_in,
-                 self.number_out]
-                + self.size_markers)
-
-    def __str__(self):
-        return 'p: {}'.format(self.panchenko())
-
-    def _extract_line(self, src, size, tstamp):
-        '''aggregates stream values'''
-        if src == HOME_IP: # outgoing packet
-            self.bytes_out += int(size)
-            self.packets_out += 1
-            self.packets.append(int(size))
-            self.timing.append((tstamp, int(size)))
-        else: #incoming
-            self.bytes_in += int(size)
-            self.packets_in += 1
-            self.packets.append(- int(size))
-            self.timing.append((tstamp, -int(size)))
-
-        self.last_time = tstamp
-
-    def _postprocess(self):
-        '''sums up etc collected features'''
-        if hasattr(self, "size_markers"):
-            return
-
-        self.size_markers = [size / 600 for size in _sum_stream(self.packets)]
-        # html response marker, thus request must have been sent before
-        if self.size_markers[1] < 0:
-            self.html_marker = - self.size_markers[1]
-        else:
-            self.html_marker = - self.size_markers[2]
-        self.total_transmitted_bytes_in = self.bytes_in / 10000
-        self.total_transmitted_bytes_out = self.bytes_out / 10000
-        self.occuring_packet_sizes = (len(set(self.packets)) / 2) * 2
-        self.percentage = (100 * self.packets_in / (self.packets_in + self.packets_out) / 5) * 5
-        self.number_in = self.packets_in / 15
-        self.number_out = self.packets_out / 15
-
-    def to_json(self):
-        '''prints packet trace to json, for reimport'''
-        return json.dumps(self.__dict__)
-
-def get_counters(*argv):
-    '''get called as main, either creates counters for the arguments, or
-    all files in this directory and below'''
-    import doctest
-    doctest.testmod()
-
-    logging.basicConfig(format='%(levelname)s:%(message)s', level=LOGLEVEL)
-
-    counters = {}
-    if len(argv) > 1:
-        for f in argv[1:]:
-            _append_features(counters, f)
-    else:
-        for (dirpath, dirnames, filenames) in os.walk(os.getcwd()):
-            l = len(filenames)
-            for i,f in enumerate(filenames):
-                fullname = os.path.join(dirpath, f)
-                if TIME_SEPARATOR in fullname: # file like google.com@1445350513
-                    logging.info('processing %s (%d/%d)', fullname, i, l)
-                    _append_features(counters, fullname);
-
-    return counters
 
 def to_features(counters):
     '''transforms counter data to feature vector pair (X,y)'''
     X_in = []
-    y = []
+    out_y = []
     feature = 0
     for domain, dom_counters in counters.iteritems():
         feature += 1
         for count in dom_counters:
             if not count.warned:
                 X_in.append(count.panchenko())
-                y.append(feature)
+                out_y.append(feature)
             else:
                 logging.warn('%s: one discarded', domain)
     # all to same length
     max_len = max([len(x) for x in X_in])
     for x in X_in:
-        _enlarge(x, max_len)
+        _pad(x, max_len)
     # traces into X, y
-    return (np.array(X_in), np.array(y))
+    return (np.array(X_in), np.array(out_y))
 
 
 if __name__ == "__main__":
-    counters = get_counters(argv)
-    (X, y) = to_features(counters)
+    doctest.testmod()
+    logging.basicConfig(format='%(levelname)s:%(message)s', level=LOGLEVEL)
+
+    (X, y) = to_features(counter.Counter.from_(sys.argv))
 
     # svm
     svc = svm.SVC(kernel='linear')
