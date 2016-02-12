285e5c6f4559f15b7d1ceeaeee86d751b128403c
kreikenbaum <kreikenbaum@users.noreply.github.com>
2016-02-12 11:14:51 +0100

    code: coverTraffic had no object + constructor, one var was not camelCased; thesis: wf was less elaborate; lit: wt lacked one section, model lacked sec 6, pan16 lacked sec 1

--- cover/js/coverTraffic.js
+++ cover/js/coverTraffic.js
@@ -2,8 +2,6 @@
 
 exports.DOC = 'creates cover traffic, up to predetermined parameters';
 
-const { setTimeout } = require("sdk/timers");
-
 const coverUrl = require("./coverUrl.js");
 const debug = require("./debug.js");
 const stats = require("./stats.js");
@@ -11,6 +9,35 @@ const stats = require("./stats.js");
 // td: changeable by pref (slider?)
 /** overhead of dummy traffic -1; 1.5 has overhead of 50% */
 const FACTOR = 1.5;
+
+function CoverTraffic(load) {
+    this.load = load;
+
+    this.site = {};
+    this.pad = {};
+
+    this.site.html = stats.htmlSize(1); // td: buffer known sites in bloomfilter
+    this.pad.html = stats.htmlSize(FACTOR) - this.site.html;
+    this.site.numEmbedded = stats.numberEmbeddedObjects(1); // td: see above
+    this.pad.numEmbedded
+	= stats.numberEmbeddedObjects(FACTOR) - this.site.numEmbedded;
+
+    this.prob = Math.max(this.pad.numEmbedded / this.site.numEmbedded, 0);
+
+    load.http(coverUrl.sized(this.pad.html));
+}
+// td: 
+CoverTraffic.prototype.loadNext = function() {
+    var i = (stats.withProbability( this.prob % 1 )
+	     ? Math.ceil(this.prob)
+	     : Math.floor(this.prob));
+    for ( ; i >= 0; i -= 1 ) {
+	if ( i > 1 || stats.withProbability(i) ) {
+	    load.http(coverUrl.sized(stats.embeddedObjectSize()));
+	}
+    }
+};
+exports.CoverTraffic = CoverTraffic;
 
 var load;
 
@@ -32,9 +59,9 @@ function start() {
 
     site_.html = stats.htmlSize(1); // td: buffer known sites in bloomfilter
     pad_.html = stats.htmlSize(FACTOR) - site_.html;
-    site_.num_embedded = stats.numberEmbeddedObjects(1); // td: see above
-    pad_.num_embedded = stats.numberEmbeddedObjects(FACTOR) - site_.num_embedded;
-    prob_ = Math.max(pad_.num_embedded / site_.num_embedded, 0);
+    site_.numEmbedded = stats.numberEmbeddedObjects(1); // td: see above
+    pad_.numEmbedded = stats.numberEmbeddedObjects(FACTOR) - site_.numEmbedded;
+    prob_ = Math.max(pad_.numEmbedded / site_.numEmbedded, 0);
 
     //    setTimeout(loadNext, stats.parsingTime());
     load.http(coverUrl.sized(pad_.html));
@@ -46,15 +73,15 @@ exports.start = start;
 // td: timeout
 function loadNext() {
     var i;
-    if ( pad_.num_embedded > 0 ) {
+    if ( pad_.numEmbedded > 0 ) {
 	for ( i = prob_; i >= 0; i -= 1 ) {
 	    if ( i > 1 || stats.withProbability(i) ) {
 		load.http(coverUrl.sized(stats.embeddedObjectSize()));
-		pad_.num_embedded -= 1;
+		pad_.numEmbedded -= 1;
 	    }
 	}
     } else {
-	console.log("cover traffic empty, num: " + pad_.num_embedded);
+	console.log("cover traffic empty, num: " + pad_.numEmbedded);
     }
 }
 exports.loadNext = loadNext;
