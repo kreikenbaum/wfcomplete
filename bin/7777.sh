1409b3ac4a28c1a45ec068509131b6a019a5a58c
kreikenbaum <kreikenbaum@users.noreply.github.com>
2016-05-22 09:16:48 +0200

    code: 7777 removal was less integrated, lit: was less sorted, (em), mitchell lacked sec 5.3.4, as lacked sec 4.2, (org)

--- bin/7777.sh
+++ bin/7777.sh
@@ -1,3 +1,12 @@
 for NAME in $(ls | grep -vE '$filtered_'); do
     tshark -r $NAME -2 -R "tcp.dstport ne 7777 and tcp.srcport ne 7777" -w filtered_$NAME;
 done
+mkdir filtered
+mv filtered_* filtered
+cd filtered
+for i in $(find . -name 'filtered_*'); do
+    mv $i $(echo $i | sed 's/filtered_//g');
+done;
+xmessage 'removing 7777 done'
+
+
