7f2cc94571ade1b1c2faf545b0dd051a37c89dbc
kreikenbaum <kreikenbaum@users.noreply.github.com>
2016-08-10 09:29:02 +0200

    plot: plot layout was side-by-side, num_embedded had no title (and lacked file name and save), thesis: design was partly less checked

--- bin/plot/plot_html_embedded.py
+++ bin/plot/plot_html_embedded.py
@@ -23,13 +23,13 @@ mu_embedded = 7.51384
 embedded = stats.lognorm(s=sigma_embedded, scale=math.exp(mu_embedded))
 
 # td: label for red and black
-plt.subplot(121)
+plt.subplot(211)
 
-plt.plot(x, html.pdf(x), 'k', label="HTML page sizes")
-plt.plot(x, embedded.pdf(x), 'r', label="embedded object sizes")
+plt.plot(x, html.pdf(x), 'k', label="HTML pages")
+plt.plot(x, embedded.pdf(x), 'r', label="embedded objects")
 plt.xlabel('size (Byte)')
 plt.ylabel('probability')
-plt.title('probability density of sizes')
+plt.title('probability density of object sizes')
 plt.legend()
 
 x2 = np.linspace(0, 40, 100)
@@ -38,7 +38,9 @@ kappa=0.141385
 theta=40.3257
 num_embedded = stats.gamma(kappa, scale=theta)
 
-plt.subplot(122)
+plt.subplot(212)
 plt.plot(x2, num_embedded.pdf(x2))
-plt.xlabel('number of embedded elements')
+plt.xlabel('number of elements')
+plt.title('probability density of number of embedded elements')
 plt.show()
+plt.savefig('/tmp/fig_html_embedded.pgf')
