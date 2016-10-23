53cb92caf3286eae81f8517b65cae2864dd8fa37
kreikenbaum <kreikenbaum@users.noreply.github.com>
2016-10-23 22:36:34 +0200

    thesis: was bit less checked, lacked linear-separability pic, top-100 was less written, lit: (mpl)

--- bin/flearn_params.sh
+++ bin/flearn_params.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-### output: 0 1 number_of_sites, training_ and testing_instances for flearner
-SITES=$(( $(ls batch | cut -d - -f 1 | sort | tail -1) +1 ))
+### output: 0 1 number_of_sites, training_ and testing_nstances for flearner
+SITES=$(( $(ls batch | cut -d - -f 1 | sort -n | tail -1) +1 ))
 INSTANCES=$(( $(ls batch | grep -v 'f$' | cut -d '-' -f 2 | sort  -n | uniq -c | grep "^[[:space:]]*$SITES" | tail -1 | tr -s ' ' | cut -d ' ' -f 3) +1))
 TEST_I=$(( $INSTANCES /3 + 1))
 TRAIN_I=$(( $INSTANCES - $TEST_I ))
