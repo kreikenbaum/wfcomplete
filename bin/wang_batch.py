f98c068b742b5f3968925e0204948c67f5e9e8d0
kreikenbaum <kreikenbaum@users.noreply.github.com>
2016-11-23 17:13:38 +0100

    code: typo, to_wang failed too loudly on existing dir, data was old-sorted, thesis: knn-classifier was less written, eva had checked less, original classification had older formatting, lit: had older order, (vds), iml lacked bit sec 3.3, timing lacked bit on sec 3, or lacked secs 5.1-4, parrot lacked sec 3

--- bin/wang_batch.py
+++ bin/wang_batch.py
@@ -6,4 +6,8 @@ import counter
 if __name__ == "__main__":
     if len(sys.argv) == 1:
         sys.argv.append('.')
-    counter.dir_to_wang(sys.argv[1])
+    try:
+        counter.dir_to_wang(sys.argv[1])
+    except OSError:
+        print("dir already exists")
+        system.exit()
