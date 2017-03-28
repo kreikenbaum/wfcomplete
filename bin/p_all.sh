12f6b8984ead5a675a57eb160e49c6bea5d03041
kreikenbaum <kreikenbaum@users.noreply.github.com>
2017-03-28 21:09:19 +0200

    lit: was less moved, into2data-analysis lacked lesson

--- bin/p_all.sh
+++ bin/p_all.sh
@@ -1,4 +1,6 @@
 P_PATH=~/da/git/sw/p
+#SVM_PATH=$P_PATH
+SVM_PATH=$P_PATH/../libsvm-3.22
 TARGET=p_batch
 
 if [ $# -ge 1 ]; then
@@ -15,5 +17,6 @@ echo "$INSTANCES instances for each class" > $($P_PATH/../../bin/path_filename.s
 python $P_PATH/feature-scripts/outlier-removal.py -in ./$TARGET/ -out ./or/ -setting CW -randomInstances NO -instances $INSTANCES -referenceFormat tcp -outlierRemoval Simple -ignoreOutlier NO
 python $P_PATH/feature-scripts/generate-feature.py -in ./or/ -out ./output/ -setting CW -classifier CUMULATIVE -force NO -features 100 -randomInstances NO -dataSet CW -instances $INSTANCES
 # last two might be better done by eval.py
-$P_PATH/libsvm-3.20-src/svm-scale -l 0 output/CW_TCP > output/CW_TCP_scaled
-nice $P_PATH/libsvm-3.20-src/tools/grid.py -log2c 0,24,4 -log2g -15,5,4 -v 3 -gnuplot "null" ./output/CW_TCP_scaled >> $($P_PATH/../../bin/path_filename.sh p-cumul.out)
+$SVM_PATH/svm-scale -l 0 output/CW_TCP > output/CW_TCP_scaled
+python -i $SVM_PATH/tools/grid.py -log2c 0,24,4 -log2g -15,5,4 -v 3 -gnuplot "null" ./output/CW_TCP_scaled >> $($P_PATH/../../bin/path_filename.sh p-cumul.out)
+#nice $SVM_PATH/tools/grid.py -log2c 0,24,4 -log2g -15,5,4 -v 3 -gnuplot "null" ./output/CW_TCP_scaled >> $($P_PATH/../../bin/path_filename.sh p-cumul.out)
