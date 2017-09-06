1fc8ad1b743ba7f0228a9d3e7950bda5b46c7519
kreikenbaum <kreikenbaum@users.noreply.github.com>
2017-09-06 13:14:29 +0200

    open-world.py had typo

--- bin/open-world.py
+++ bin/open-world.py
@@ -8,8 +8,9 @@ import scenario
 
 if __name__ == "__main__":
     logging.basicConfig(format=analyse.LOGFORMAT, level=analyse.LOGLEVEL)
+    os.nice(20)
     if len(sys.argv) == 1:
         analyse.open_world(scenario.Scenario(os.getcwd()))
     elif len(sys.argv) > 2:
         logging.warn('only first scenario chosen for open world analysis')
-    analyse.open_world(scenario.Scenario(sys.argv[1]), smart=True)
+    analyse.open_world(scenario.Scenario(sys.argv[1], smart=True))
