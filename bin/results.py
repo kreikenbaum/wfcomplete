7607cd5ae93cf97b7a4c6e4ae5d6d0959147e26e
kreikenbaum <kreikenbaum@users.noreply.github.com>
2017-09-20 14:06:29 +0200

    retrieve lacked default param

--- bin/results.py
+++ bin/results.py
@@ -11,26 +11,27 @@ client = pymongo.MongoClient()
 db = client.sacred
 
 class Result(object):
-    def __init__(self, scenario_, accuracy, git):
+    def __init__(self, scenario_, accuracy, git, time):
         self.scenario = scenario_
-        self.accuracy = accuracy
+        self.cumul = accuracy
         self.git = git
+        self.time = time
 
     @staticmethod
     def from_mongoentry(entry):
         return Result(scenario.Scenario(entry['config']['scenario']),
                       entry['result']['score'],
-                      entry['experiment']['repositories'][0]['commit'])
+                      entry['experiment']['repositories'][0]['commit'],
+                      entry['stop_time'])
+
+    def __add__(self, other):
+        return Result(None, self.accuracy + other.accuracy, None, None)
+
+    def save(self):
+        '''saves entry to mongodb'''
 
 if __name__ == "__main__":
     resulting = db.runs.find({"$and": [{"config.scenario": {"$exists": 1}}, {"result.score": {"$exists": 1}}]})
-    results = [Result.from_mongoentry(x) for x in resulting]
-
-
-
-
-
-
-
-
-    
+    all_ = [Result.from_mongoentry(x) for x in resulting]
+    ## todo: filter to get only one element per scenario
+    ### here or mongo?
