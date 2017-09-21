fa463fef9947fc6c3a0343a0d4db5fbdb8233f2a
kreikenbaum <kreikenbaum@users.noreply.github.com>
2017-09-21 10:54:36 +0200

    data: export was more tedious, results.py: lacked import method

--- bin/results.py
+++ bin/results.py
@@ -3,12 +3,13 @@
 target usage: load from different sources (or upload all to db),
 then compute mean and variance/std
 '''
+import csv
+import datetime
+import logging
+
 import pymongo
 
 import scenario
-
-client = pymongo.MongoClient()
-db = client.sacred
 
 class Result(object):
     def __init__(self, scenario_, accuracy, git, time):
@@ -27,11 +28,38 @@ class Result(object):
     def __add__(self, other):
         return Result(None, self.accuracy + other.accuracy, None, None)
 
+    def __repr__(self):
+        return '<Result({}, {}, {}, {})>'.format(
+            self.scenario, self.cumul, self.git, self.time)
+
     def save(self):
         '''saves entry to mongodb'''
+        db.runs.insert_one({"config": {"scenario": self.scenario},
+                            "result": {"score": self.cumul},
+                            "stop_time": self.time,
+                            "status": "EXTERNAL"})
+
+def import_to_mongo(csvfile):
+    imported = []
+    for el in csv.DictReader(csvfile):
+        try:
+            date = datetime.datetime.strptime(el['notes'], '%Y-%m-%d').date()
+        except ValueError:
+            date = None
+        if el['cumul']:
+            imported.append(Result(el['defense'], el['cumul'], None, date))
+        else:
+            logging.warn("skipped {}".format(str(el)))
+    for el in imported:
+        print el
+        #el.save()
 
 if __name__ == "__main__":
-    resulting = db.runs.find({"$and": [{"config.scenario": {"$exists": 1}}, {"result.score": {"$exists": 1}}]})
-    all_ = [Result.from_mongoentry(x) for x in resulting]
+    client = pymongo.MongoClient()
+    db = client.sacred
+    all_ = [Result.from_mongoentry(x) for x in
+            db.runs.find({"$and": [{"config.scenario": {"$exists": 1}},
+                                   {"result.score": {"$exists": 1}}]})]
     ## todo: filter to get only one element per scenario
     ### here or mongo?
+    # import_to_mongo(open('/home/uni/da/git/data/results/10sites.csv'))
