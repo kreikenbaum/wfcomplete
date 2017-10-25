27d0cd946b533ccb26f88b0035e7a05e0de37e72
kreikenbaum <kreikenbaum@users.noreply.github.com>
2017-10-25 11:06:21 +0200

    lacked db-import

---
+++ bin/importDucksteinDB.py
@@ -0,0 +1,22 @@
+#! /usr/bin/env/python
+'''imports experiment data from duckstein into local db'''
+import json
+import results
+import subprocess
+
+runs = subprocess.check_output(
+    ["ssh", "mkreik@duckstein", "mongoexport -d sacred -c runs"])
+
+for line in runs.split('\n'):
+    if 'COMPLETED' in line:
+        entry = json.loads(line)
+        entry['_id'] = results._next_id()
+        imp = subprocess.Popen(["mongoimport", "-c", "runs", "-d", "sacred"],
+                               stdin=subprocess.PIPE)
+        imp.communicate(input=json.dumps(entry))
+
+subprocess.call(
+    ["ssh", "mkreik@duckstein",
+     'mongo sacred --eval "printjson(db.dropDatabase())"'])
+
+
