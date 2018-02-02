11305b3a36d1e5e65df9158b5fdde86ae6c3ec50
kreikenbaum <kreikenbaum@users.noreply.github.com>
2018-02-02 14:58:39 +0100

    wsgi lacked usage

---
+++ bin/capture/wsgi.py
@@ -0,0 +1,47 @@
+'''Simple wsgi module which creates a random string of size 'size'.
+
+Requires the python package "mod_wsgi". Usage:
+$ mod_wsgi-express start-server path/to/wsgi.py [--port 8000]
+$ sudo mod_wsgi-express start-server --user nobody path/to/wsgi.py --port 80
+
+Get data: curl host:port/size?300 for 300 random (-header) bytes.
+'''
+import logging
+import random
+import string
+
+LOGFORMAT='%(levelname)s:%(filename)s:%(lineno)d:%(message)s'
+logging.basicConfig(format=LOGFORMAT, level=logging.INFO)
+
+
+def application(environ, start_response):
+    status = '200 OK'
+    logging.info('got request: ' + environ['QUERY_STRING'])
+
+    output = _randomString(_reduceSize(_getSize(environ['QUERY_STRING'])))
+
+    response_headers = [('Content-type', 'text/plain'),
+                        ('Content-Length', str(len(output)))]
+    start_response(status, response_headers)
+
+    return [output]
+
+# this depends on if "Proxy-Connection: keep-alive" is included, better safe...
+def _reduceSize(size):
+    '''reduces size by header length'''
+    my_len = size -135 -len(str(size))
+    my_len += len(str(size)) - len(str(my_len))
+    logging.debug('size: %d', my_len)
+    return my_len
+
+def _getSize(query):
+    '''extracts value of size parameter'''
+    for parampair in query.split('&'):
+        (var, val) = parampair.split('=')
+        if var == 'size':
+            return max(1, int(val))
+    raise IndexError('no size parameter')
+
+def _randomString(size):
+    '''generate random string of size "size"'''
+    return ''.join(random.choice(string.printable) for _ in range(size))
