87cbc48697bddb7044839e7ae5a8e00b31282af5
kreikenbaum <kreikenbaum@users.noreply.github.com>
2018-03-16 18:26:29 +0100

    exp-db import code was less configurable

--- bin/importDucksteinDB.py
+++ bin/importDucksteinDB.py
@@ -1,32 +1,45 @@
 #! /usr/bin/env python
-'''imports experiment data from duckstein into local db'''
+'''imports experiment data from host (default:duckstein) into local db
+
+this needs mongoexport and mongo to be in path'''
 import json
 import os
 import results
 import subprocess
+import sys
 
 import pymongo
 
-try:
-    pymongo.MongoClient(serverSelectionTimeoutMS=10).server_info()
-except pymongo.errors.ServerSelectionTimeoutError:
-    print 'start local mongodb!'
-    exit(1)
-
-runs = subprocess.check_output(
-    ["ssh", "mkreik@duckstein", "mongoexport -d sacred -c runs"])
-
-for line in runs.split('\n'):
-    if 'COMPLETED' in line:
-        entry = json.loads(line)
-        entry['_id'] = results._next_id()
-        imp = subprocess.Popen(["mongoimport", "-c", "runs", "-d", "sacred"],
-                               stdin=subprocess.PIPE)
-        imp.communicate(input=json.dumps(entry))
-
-os.system("ssh mkreik@duckstein 'mongoexport -d sacred -c runs > /home/mkreik/data/mongodump$(date +%F_%T)'")
-subprocess.call(
-    ["ssh", "mkreik@duckstein",
-     'mongo sacred --eval "printjson(db.dropDatabase())"'])
+DEFAULT_LOGIN = "mkreik@duckstein"
 
 
+def load(ssh_login):
+    try:
+        pymongo.MongoClient(serverSelectionTimeoutMS=10).server_info()
+    except pymongo.errors.ServerSelectionTimeoutError:
+        print 'start local mongodb!'
+        exit(1)
+
+    runs = subprocess.check_output(
+        ["ssh", DEFAULT_LOGIN, "mongoexport -d sacred -c runs"])
+
+    for line in runs.split('\n'):
+        if 'COMPLETED' in line:
+            entry = json.loads(line)
+            entry['_id'] = results._next_id()
+            imp = subprocess.Popen(
+                ["mongoimport", "-c", "runs", "-d", "sacred"],
+                stdin=subprocess.PIPE)
+            imp.communicate(input=json.dumps(entry))
+
+    os.system("ssh {} 'mongoexport -d sacred -c runs > /home/mkreik/data/mongodump$(date +%F_%T)'".format(DEFAULT_LOGIN))
+    subprocess.call(
+        ["ssh", DEFAULT_LOGIN,
+         'mongo sacred --eval "printjson(db.dropDatabase())"'])
+
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        load(sys.argv[1])
+    else:
+        load(DEFAULT_LOGIN)
