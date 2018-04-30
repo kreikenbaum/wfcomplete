3fd6ba18580918388ec819eaf45d8e8f5218bc2f
kreikenbaum <kreikenbaum@users.noreply.github.com>
2018-04-30 09:41:36 +0200

    status.sh produced no valid JSON if delay was set, or factor not set
    - one_site had old firefox hack
    - thesis lacked bits (longtable + sample scenario code)

--- bin/capture/get_delay.sh
+++ bin/capture/get_delay.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
 ### prints connection delay
-DELAY=$(/sbin/tc qdisc | grep ens18 | grep delay | sed 's/.*\(delay.*\)/\1/g' | cut -d ' ' -f 2)
+DELAY=$(/sbin/tc qdisc | grep ens18 | grep delay | sed 's/.*\(delay.*\)/\1/g' | cut -d ' ' -f 2 | sed 's/ms//g')
 if [ x$DELAY = x ]; then DELAY=0; fi
 echo $DELAY
--- bin/capture/get_factor.sh
+++ bin/capture/get_factor.sh
@@ -1,2 +1,4 @@
-get_pref.sh 'extensions.@wf-cover.factor'
+### gets configuration of addon: factor
+DEFAULT=50
+get_pref.sh 'extensions.@wf-cover.factor' | sed "s/null/$DEFAULT/g"
 
